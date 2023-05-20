39d399d589020b4b89bbb554b57047fe3bdc28f6
leeviallu <68004427+leeviallu@users.noreply.github.com>
2023-05-20 15:18:15 +0300

    Tiedoston siistimistä

--- qraphql-library-backend/index.js
+++ qraphql-library-backend/index.js
@@ -18,28 +18,14 @@ let authors = [
         born: 1821,
     },
     {
-        name: "Joshua Kerievsky", // birthyear not known
+        name: "Joshua Kerievsky",
         id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
     },
     {
-        name: "Sandi Metz", // birthyear not known
+        name: "Sandi Metz",
         id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
     },
 ];
-
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conección con el libro
- */
 
 let books = [
     {
