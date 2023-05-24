e34f4e1474fe7e03c3191c2ba819106715b4038a
leeviallu <68004427+leeviallu@users.noreply.github.com>
2023-05-24 21:23:37 +0300

    Kirjojen lista korjattu

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,14 +1,19 @@
 import { useQuery } from "@apollo/client";
 import { ALL_BOOKS } from "../queries";
+import { ALL_AUTHORS } from "../queries";
 
 const Books = () => {
-    const result = useQuery(ALL_BOOKS, {
+    const booksQuery = useQuery(ALL_BOOKS, {
         pollInterval: 2000,
     });
-    if (result.loading) {
+    const authorsQuery = useQuery(ALL_AUTHORS, {
+        pollInterval: 2000,
+    });
+    if (booksQuery.loading || authorsQuery.loading) {
         return null;
     }
-    const books = result.data.allBooks;
+    const books = booksQuery.data.allBooks;
+    const authors = authorsQuery.data.allAuthors;
     return (
         <div>
             <h2>books</h2>
@@ -20,13 +25,18 @@ const Books = () => {
                         <th>author</th>
                         <th>published</th>
                     </tr>
-                    {books.map((a) => (
-                        <tr key={a.title}>
-                            <td>{a.title}</td>
-                            <td>{a.author}</td>
-                            <td>{a.published}</td>
-                        </tr>
-                    ))}
+                    {books.map((a) => {
+                        const author = authors.find((author) => {
+                            return a.author === author.id;
+                        });
+                        return (
+                            <tr key={a.title}>
+                                <td>{a.title}</td>
+                                <td>{author.name}</td>
+                                <td>{a.published}</td>
+                            </tr>
+                        );
+                    })}
                 </tbody>
             </table>
         </div>
