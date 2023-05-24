bbd1d18edc5910bd00f1a0ed00130968573d3bfb
leeviallu <68004427+leeviallu@users.noreply.github.com>
2023-05-25 00:36:35 +0300

    Genren mukaan renderöinti

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,23 +1,46 @@
+import { useState } from "react";
 import { useQuery } from "@apollo/client";
 import { ALL_BOOKS } from "../queries";
 import { ALL_AUTHORS } from "../queries";
 
 const Books = () => {
-    const booksQuery = useQuery(ALL_BOOKS, {
+    const [genre, setGenre] = useState("");
+    const allGenres = [];
+    const allBooksQuery = useQuery(ALL_BOOKS, {
+        pollInterval: 2000,
+    });
+    const filteredBooksQuery = useQuery(ALL_BOOKS, {
+        variables: { genre },
         pollInterval: 2000,
     });
     const authorsQuery = useQuery(ALL_AUTHORS, {
         pollInterval: 2000,
     });
-    if (booksQuery.loading || authorsQuery.loading) {
+    if (
+        allBooksQuery.loading ||
+        filteredBooksQuery.loading ||
+        authorsQuery.loading
+    ) {
         return null;
     }
-    const books = booksQuery.data.allBooks;
+    const filteredBooks = filteredBooksQuery.data.allBooks;
     const authors = authorsQuery.data.allAuthors;
+    const { allBooks } = allBooksQuery.data;
+    allBooks.forEach((book) => {
+        book.genres.forEach((genre) => {
+            if (!allGenres.includes(genre)) {
+                allGenres.push(genre);
+            }
+        });
+    });
     return (
         <div>
             <h2>books</h2>
-
+            {genre ? (
+                <p>
+                    in genre <b>{genre}</b>
+                </p>
+            ) : null}
             <table>
                 <tbody>
                     <tr>
@@ -25,7 +48,7 @@ const Books = () => {
                         <th>author</th>
                         <th>published</th>
                     </tr>
-                    {books.map((a) => {
+                    {filteredBooks.map((a) => {
                         const author = authors.find((author) => {
                             return a.author === author.id;
                         });
@@ -39,6 +62,14 @@ const Books = () => {
                     })}
                 </tbody>
             </table>
+            {allGenres.map((genre, i) => {
+                return (
+                    <button key={i} onClick={() => setGenre(genre)}>
+                        {genre}
+                    </button>
+                );
+            })}
+            <button onClick={() => setGenre("")}>all genres</button>
         </div>
     );
 };
--- library-frontend/src/queries.js
+++ library-frontend/src/queries.js
@@ -12,8 +12,8 @@ export const ALL_AUTHORS = gql`
 `;
 
 export const ALL_BOOKS = gql`
-    query {
-        allBooks {
+    query Query($genre: String) {
+        allBooks(genre: $genre) {
             title
             author
             published
