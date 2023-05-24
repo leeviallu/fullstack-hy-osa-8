35bfae5fa3e507fd748d39347ffea3e258920472
leeviallu <68004427+leeviallu@users.noreply.github.com>
2023-05-24 18:14:46 +0300

    db toimii resolvereitten osalta

--- qraphql-library-backend/index.js
+++ qraphql-library-backend/index.js
@@ -21,84 +21,6 @@ mongoose
         console.log("error connection to MongoDB:", error.message);
     });
 
-let authors = [
-    {
-        name: "Robert Martin",
-        id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-        born: 1952,
-    },
-    {
-        name: "Martin Fowler",
-        id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-        born: 1963,
-    },
-    {
-        name: "Fyodor Dostoevsky",
-        id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-        born: 1821,
-    },
-    {
-        name: "Joshua Kerievsky",
-        id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-    },
-    {
-        name: "Sandi Metz",
-        id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-    },
-];
-
-let books = [
-    {
-        title: "Clean Code",
-        published: 2008,
-        author: "Robert Martin",
-        id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-        genres: ["refactoring"],
-    },
-    {
-        title: "Agile software development",
-        published: 2002,
-        author: "Robert Martin",
-        id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-        genres: ["agile", "patterns", "design"],
-    },
-    {
-        title: "Refactoring, edition 2",
-        published: 2018,
-        author: "Martin Fowler",
-        id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-        genres: ["refactoring"],
-    },
-    {
-        title: "Refactoring to patterns",
-        published: 2008,
-        author: "Joshua Kerievsky",
-        id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-        genres: ["refactoring", "patterns"],
-    },
-    {
-        title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
-        published: 2012,
-        author: "Sandi Metz",
-        id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-        genres: ["refactoring", "design"],
-    },
-    {
-        title: "Crime and punishment",
-        published: 1866,
-        author: "Fyodor Dostoevsky",
-        id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-        genres: ["classic", "crime"],
-    },
-    {
-        title: "The Demon ",
-        published: 1872,
-        author: "Fyodor Dostoevsky",
-        id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-        genres: ["classic", "revolution"],
-    },
-];
-
 const typeDefs = `
   type Query {
     bookCount: Int
@@ -137,21 +59,10 @@ const resolvers = {
     Query: {
         bookCount: () => Book.collection.countDocuments(),
         authorCount: () => Author.collection.countDocuments(),
-        allBooks: (root, args) => {
+        allBooks: async (root, args) => {
             if (!args.author && !args.genre) {
-                return books;
+                return Book.find({});
             }
-            const byAuthor = (book) =>
-                args.author === book.author ? book.author : !book.author;
-
-            const byGenre = (book) => {
-                for (key in book.genres) {
-                    if (book.genres[key].includes(args.genre)) {
-                        return book;
-                    }
-                }
-            };
-
             if (
                 !(
                     args.author === undefined ||
@@ -160,33 +71,41 @@ const resolvers = {
                     args.genre === null
                 )
             ) {
-                const filterByAuthor = books.filter(byAuthor);
-                const filterByGenre = filterByAuthor.filter(byGenre);
-                return filterByGenre;
+                const author = await Author.findOne({ name: args.author });
+                const books = await Book.find({
+                    author: author.id,
+                    genres: args.genre,
+                });
+                return books;
             }
             if (!(args.author === undefined || args.author === null)) {
-                return books.filter(byAuthor);
+                const author = await Author.findOne({ name: args.author });
+                const books = await Book.find({
+                    author: author.id,
+                });
+                return books;
             }
             if (!(args.genre === undefined || args.genre === null)) {
-                return books.filter(byGenre);
+                const books = await Book.find({
+                    genres: args.genre,
+                });
+                return books;
             }
         },
         allAuthors: async () => await Author.find({}),
     },
     Author: {
         name: (root) => root.name,
-        bookCount: (root) => {
-            const authorBooks = books.filter(
-                (book) => book.author === root.name
-            );
-            return authorBooks.length;
+        bookCount: async (root) => {
+            const books = await Book.find({ author: root.name });
+            return books.length;
         },
         id: (root) => root.id,
         born: (root) => root.born,
     },
     Mutation: {
         addBook: async (root, args) => {
-            let author = await Author.collection.findOne({
+            let author = await Author.findOne({
                 name: args.author,
             });
             if (!author) {
